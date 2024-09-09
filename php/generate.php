4a27bf16c58e06fcbf5037692bcbff74b896f43d
yoshiya-m <2101115016rz@cyber-u.ac.jp>
2024-09-09 21:45:48 +0900

    second update

--- php/generate.php
+++ php/generate.php
@@ -6,17 +6,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $inputContent = $_POST['inputText'] ?? '';
     $requestType = $_POST['requestType'] ?? '';
 
+    // if ($requestType === "answer") {
+    //     $outputDir = ANSWER_OUTPUT_DIR_PATH;
+    // } else if($requestType === "user") {
+    //     $outputDir = USER_OUTPUT_DIR_PATH;
+    // }
+
     if ($requestType === "answer") {
         $outputDir = ANSWER_OUTPUT_DIR_PATH;
-    } else if($requestType === "user") {
+    } else {
         $outputDir = USER_OUTPUT_DIR_PATH;
     }
 
     file_put_contents(USER_INPUT_FILE_PATH, $inputContent);
+    // $commandFormat = "java -jar %s -o %s -tpng %s 2> %s";
+    // $command = sprintf($commandFormat,
+    //     PLANTUML_JAR_FILE_PATH, USER_INPUT_FILE_PATH, $outputDir, ERRORLOG_PATH);
 
-    $commandFormat = "java -jar %s %s -tpng -o %s 2> %s";
+    $commandFormat = "java -jar %s -o %s -tpng %s";
     $command = sprintf($commandFormat,
-        PLANTUML_JAR_FILE_PATH, USER_INPUT_FILE_PATH, $outputDir, ERRORLOG_PATH);
+        PLANTUML_JAR_FILE_PATH, $outputDir, USER_INPUT_FILE_PATH);
 
     // コマンドを実行し、出力とエラーを取得
     $output = [];
